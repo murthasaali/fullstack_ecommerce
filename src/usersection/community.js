e3bb9dca9991337a8b922d2c7ef4e7d3ff3183f3
murthasaali <murthasaalick123@gmail.com>
2024-02-24 23:33:01 +0530

    posts home designed

--- src/usersection/community.js
+++ src/usersection/community.js
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaHeart, FaHouse, FaUser } from 'react-icons/fa6';
 import { MdChat, MdEdit, MdSaveAlt, MdShare } from 'react-icons/md';
 import { FiAlignCenter } from "react-icons/fi";
@@ -19,14 +19,14 @@ function Community() {
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
-      };
+    };
 
-    useEffect(()  => {
+    useEffect(() => {
         const fetchPosts = async () => {
             try {
                 const res = await getAllPosts();
                 console.log("data", res);
-                setPosts(res.latestPosts); 
+                setPosts(res.latestPosts);
                 // Assuming `latestPosts` is the key containing posts in your response
             } catch (error) {
                 console.error('Error fetching posts:', error);
@@ -36,13 +36,13 @@ function Community() {
         fetchPosts();
     }, [])
 
-    console.log("posts",posts)
-    
+    console.log("posts", posts)
+
 
     const renderContent = () => {
         switch (activeButton) {
             case 'house':
-                return <> <div className='w-full h-auto flex gap-2 border-black border-b-[1px] py-2 '>
+                return <> <div className='w-full h-auto flex gap-2  py-3 '>
                     <div className='md:w-20 w-12 md:h-20 h-12 rounded-full bg-stone-50 bg-opacity-25 border-black '>
 
                     </div>
@@ -62,59 +62,73 @@ function Community() {
 
                     </div>
                 </div>
-                  
-                {
-    posts.map((item) => {
-        return (
-            <div className='w-full pb-20    h-auto p-1 flex flex-col mx-[10px]' key={item._id}>
 
-                <div className='w-full  h-16  flex justify-between items-center'>
-                    <div className=' flex items-end gap-[10px]'>
-                        <div className='w-10 h-10  bg-white rounded-full overflow-hidden'>
-                            <img src={item.postedBy?item.postedBy.image:pro} className='h-full w-[150%] '  alt='postedby'/>
-                        </div>
-                        <span className='text-xs'>{item.postedBy?item.postedBy.email:"nothing"}</span>
-                    </div>
-                    <div className='flex items-center gap-3'>
-
-                        <span>14 h</span>
-                        <button><FaDotCircle /></button>
+                    {
+                        posts.map((item) => {
+                            return (
+                                <div className='w-full pb-20    h-auto p-1 flex flex-col mx-[10px]' key={item._id}>
 
 
-                    </div>
+                                    <div className='w-full md:h-[500px]  h-[480px] bg-stone-50 bg-opacity-50 md:gap-4 gap-1  border-s-[1px] relative rounded-3xl flex flex-col justify-center px-4 items-start'>
 
-                </div>
-                <div className='w-full md:h-[500px]  h-96 bg-stone-50 bg-opacity-50  border-s-[1px] relative rounded-3xl flex flex-col justify-center px-4 items-start'>
-                    <div className='text-xs'>{item.caption}</div>
-                    <img src={item.image} alt='posts' className='h-[70%] rounded-lg w-[90%]'/>
-                    <div className='text-xs'>{item.hashtag}</div>
-                    <div className='w-auto h-[80%] p-3 bg-stone-50 bg-opacity-50 absolute right-2 flex flex-col justify-around rounded-lg'>
-<button> <FaHeart/></button>
-<button> <MdChat/></button>
-<button> <MdShare/></button>
-<button onClick={()=>handleDownload(item.image)}>
-        <MdSaveAlt /> 
-      </button>
-                        
-
-                    </div>
-                </div>
+                                    <div className='w-auto h-[80%] p-3  bg-opacity-50 absolute right-2 md:flex hidden flex-col justify-around rounded-lg'>
+                                        <button> <FaHeart /></button>
+                                        <button> <MdChat /></button>
+                                        <button> <MdShare /></button>
+                                        <button onClick={() => handleDownload(item.image)}>
+                                            <MdSaveAlt />
+                                        </button>
 
 
-            </div>
-        );
-    })
-}
+                                    </div>
+
+                                        <div className='w-full    flex justify-between items-center '>
+                                            <div className=' flex items-end gap-[10px]'>
+                                                <div className='w-10 h-10  bg-white rounded-full overflow-hidden'>
+                                                    <img src={item.postedBy ? item.postedBy.image : pro} className='h-full w-[150%] ' alt='postedby' />
+                                                </div>
+                                                <span className='text-xs'>{item.postedBy ? item.postedBy.email : "nothing"}</span>
+                                           
+                                           
+                                            </div>
+                                            <div className='flex items-center gap-3'>
+
+                                                <span>14 h</span>
+                                                <button><FaDotCircle /></button>
+
+
+                                            </div>
+
+                                        </div>
+                                        <img src={item.image} alt='posts' className='h-[68%] rounded-lg md:w-[90%] w-full ' />
+                                        <div className='w-full h-auto p-3 flex md:hidden    justify-around rounded-lg'>
+                                            <button> <FaHeart /></button>
+                                            <button> <MdChat /></button>
+                                            <button> <MdShare /></button>
+                                            <button onClick={() => handleDownload(item.image)}>
+                                                <MdSaveAlt />
+                                            </button>
+                                        </div>
+                                       <div> <div className='text-xs'>{item.caption}</div>
+                                        <div className='text-xs'>{item.hashtag}</div></div>
+                                    </div>
+                                  
+
+
+                                </div>
+                            );
+                        })
+                    }
 
                 </>;
             case 'search':
-                return <><Search/></>;
+                return <><Search /></>;
             case 'edit':
-                return <><Notification/></>;
+                return <><Notification /></>;
             case 'heart':
-                return <><Notification/></>;
+                return <><Notification /></>;
             case 'user':
-                return <><Account/></>;
+                return <><Account /></>;
             default:
                 return null;
         }
@@ -125,34 +139,34 @@ function Community() {
             background: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)'
         }}>
             <div className='w-full backdrop-blur-lg h-auto p-2 flex md:hidden justify-evenly absolute bottom-0 z-50'>
-            <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'house' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('house')}><FaHouse  className='hover:scale-125 transition-all duration-300 '/></button>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'search' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('search')}><FaSearch  className='hover:scale-125 transition-all duration-300 '/></button>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'edit' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('edit')}><MdEdit  className='hover:scale-125 transition-all duration-300 '/></button>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'heart' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('heart')}><FaHeart  className='hover:scale-125 transition-all duration-300 '/></button>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'user' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('user')}><FaUser  className='hover:scale-125 transition-all duration-300 '/></button>
-                </div>
+                <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'house' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('house')}><FaHouse className='hover:scale-125 transition-all duration-300 ' /></button>
+                <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'search' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('search')}><FaSearch className='hover:scale-125 transition-all duration-300 ' /></button>
+                <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'edit' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('edit')}><MdEdit className='hover:scale-125 transition-all duration-300 ' /></button>
+                <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'heart' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('heart')}><FaHeart className='hover:scale-125 transition-all duration-300 ' /></button>
+                <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'user' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('user')}><FaUser className='hover:scale-125 transition-all duration-300 ' /></button>
+            </div>
             <div className='w-full h-20  mb-2 hidden md:flex bg-opacity-60  justify-between px-3 items-center'>
 
 
                 <div className='text-white text-xl font-thin'>pethouse</div>
                 <div className='h-fit w-full md:w-[500px]   md:flex hidden  justify-evenly items-center'>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'house' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('house')}><FaHouse  className='hover:scale-125 transition-all duration-300 '/></button>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'search' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('search')}><FaSearch  className='hover:scale-125 transition-all duration-300 '/></button>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'edit' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('edit')}><MdEdit  className='hover:scale-125 transition-all duration-300 '/></button>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'heart' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('heart')}><FaHeart  className='hover:scale-125 transition-all duration-300 '/></button>
-                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'user' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('user')}><FaUser  className='hover:scale-125 transition-all duration-300 '/></button>
+                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'house' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('house')}><FaHouse className='hover:scale-125 transition-all duration-300 ' /></button>
+                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'search' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('search')}><FaSearch className='hover:scale-125 transition-all duration-300 ' /></button>
+                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'edit' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('edit')}><MdEdit className='hover:scale-125 transition-all duration-300 ' /></button>
+                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'heart' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('heart')}><FaHeart className='hover:scale-125 transition-all duration-300 ' /></button>
+                    <button className={`h-fit p-3 rounded-xl bg-stone-50 bg-opacity-60 ${activeButton === 'user' ? 'bg-stone-500' : ''}`} onClick={() => setActiveButton('user')}><FaUser className='hover:scale-125 transition-all duration-300 ' /></button>
                 </div>
                 <button className='text-white  font-thin text-3xl'><FiAlignCenter /></button>
 
             </div>
-        
+
             <div className='w-full h-full  flex justify-center p-0'>
-            <div className='md:h-[600px] h-[100%] bg-stone-100 bg-opacity-40 rounded-xl w-full md:w-[750px] overflow-y-scroll flex flex-col justify-start items-center md:p-4 p-1' style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+                <div className='md:h-[600px] h-[100%] bg-stone-100 bg-opacity-40 rounded-xl w-full md:w-[750px] overflow-y-scroll flex flex-col justify-start items-center md:p-4 p-1' style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
                     {renderContent()}
                 </div>
             </div>
         </div>
     )
-}   
+}
 
 export default Community;
